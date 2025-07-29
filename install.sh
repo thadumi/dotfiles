40dd597deda763e183fe36f72d7e619da18eb1a2
Theo Dumitrescu <thadumi@outlook.com>
2025-07-29 01:20:40 +0100

    added simple install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SOURCE=$(pwd)
+
+echo 'Settings up symbolic links to repository'
+
+ln -s $SOURCE/config/.zshrc ~/.zshrc
+ln -s $SOURCE/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -s $SOURCE/config/.tmux.conf ~/.tmux.conf
