32992b2e0980d1d3707647407dbb238dd6a2a907
Theo Dumitrescu <thadumi@outlook.com>
2025-08-03 18:11:33 +0100

    added vimrc

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ echo 'Settings up symbolic links to repository'
 ln -s $SOURCE/config/.zshrc ~/.zshrc
 ln -s $SOURCE/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 ln -s $SOURCE/config/.tmux.conf ~/.tmux.conf
+ln -s ~/src/dotfiles/config/.tmux.conf ~/.vimrc
