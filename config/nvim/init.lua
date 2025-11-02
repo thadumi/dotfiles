6c343c39b8515f3e787338b998686201ccd3150b
Theo Dumitrescu <thadumi@outlook.com>
2025-11-02 16:12:14 +0000

    clangd setup

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -8,6 +8,21 @@ vim.opt.expandtab = true
 -- Show Line Numbers
 vim.wo.number = true
 
+local lspconfig = require "lspconfig"
+lspconfig["clangd"].setup {}
+
+-- Set default root markers for all clients
+--vim.lsp.config('*', {
+--  root_markers = { '.git' },
+-- })
+
+-- Set configuration for clangd language server
+-- vim.lsp.config('c_ls', {
+ -- cmd = { 'c-language-server', '--stdio' },
+ -- filetypes = { 'c' },
+--})
+
+
 -- Enable plugin manager
 -- require('lazy').setup('plugins')
 -- require("config.lazy")
