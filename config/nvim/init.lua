d95713a7783f81bea1e6ed689480ef1688ece385
Theo Dumitrescu <thadumi@outlook.com>
2025-11-02 00:58:14 +0000

    init nvim

---
+++ config/nvim/init.lua
@@ -0,0 +1,13 @@
+-- Set tabstop to 4 spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth=4
+
+-- Convert tabs to spaces
+vim.opt.expandtab = true
+
+-- Show Line Numbers
+vim.wo.number = true
+
+-- Enable plugin manager
+-- require('lazy').setup('plugins')
+-- require("config.lazy")
