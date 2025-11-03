cf6401df4d6b851fd5a502581147441a19869b35
Theo Dumitrescu <thadumi@outlook.com>
2025-11-03 23:54:39 +0000

    small improvmnets

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
+
 SOURCE=$(pwd)
 
 echo 'Settings up symbolic links to repository'
@@ -10,3 +12,16 @@ ln -s $SOURCE/config/.tmux.conf ~/.tmux.conf
 
 ln -s ~/src/dotfiles/config/.vimrc ~/.vimrc
 ln -s ~/src/dotfiles/config/nvim ~/.config/nvim
+
+#-------------------- ZSH plugins ------------------------
+echo '----------------------------------------------------'
+echo 'Installing ZSH plugins...'
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+#-------------------- NeoVim plugins -----------------------
+echo '----------------------------------------------------'
+echo 'Installing neovim plugins...'
+
+git clone https://github.com/neovim/nvim-lspconfig ~/.config/nvim/pack/nvim/start/nvim-lspconfig
